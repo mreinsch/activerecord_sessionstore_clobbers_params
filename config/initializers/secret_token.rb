73799111301785378c89f8f442ebe61e428d6174
Michael Reinsch <michael@mobalean.com>
2010-12-03 16:48:45 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arss::Application.config.secret_token = '6c0ffa4b00c991cbb41c203ec054fbb39d761af9125317229fc9cd87b22ec6b6379e67f91351a887e9d97c75eb3afdbdd732e40874cbe3ca7719152d66cf2a30'
