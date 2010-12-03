bf027c5e0f1ef7541594a46e100b46a7812248d3
Michael Reinsch <michael@mobalean.com>
2010-12-03 17:17:39 +0900

    broken

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,9 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
-Arss::Application.config.session_store :cookie_store, :key => '_dks'
+#Arss::Application.config.session_store :cookie_store, :key => '_dks'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-#Arss::Application.config.session_store :active_record_store, :key => '_dks', :cookie_only => false
+Arss::Application.config.session_store :active_record_store, :key => '_dks', :cookie_only => false
 
